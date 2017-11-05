4a98964104ca8d92a6b41e97c7bf7577fb9b9a59
Nick <nicholas.vandette@nuix.com>
2017-11-05 01:12:43 -0400

    motherfuck merge

--- actions/begin.pyw
+++ actions/begin.pyw
@@ -1,7 +1,15 @@
 import subprocess
+import sys
+import json
+sys.path.insert(0,"./game/resources/")
 import core
 
-with open("data.json") as data:
+with open("./game/resources/data.json", "r+") as data:
     gameState = json.load(data)
 
-    gameState.flag["gameStarted"] = "true"
+    gameState["flag"]["gameStarted"] = True
+
+    data.truncate()
+    data.write(json.dumps(gameState))
+
+    core.run()
--- game/resources/core.py
+++ game/resources/core.py
@@ -7,19 +7,17 @@ def open_file(path):
     if platform.system() == "Windows":
         subprocess.Popen('explorer "{0}"'.format(path))
     elif platform.system() == "Darwin":
-        os.system('open "%s"' % "actions")
+        os.system('open "%s"' % path)
     else:
         subprocess.Popen(["xdg-open", path])
 
-<<<<<<< HEAD
 def run():
-    with open("data.json") as data:
+    with open("./game/resources/data.json") as data:
         gameState = json.load(data)
-        print gameState["name"]
-=======
-with open("data.json") as data:
-    gameState = json.load(data)
-    print gameState["name"]
 
-def run():
->>>>>>> b03707551a74d87210386bc27151016fcc012e5c
+        with open("../../game.txt", "r+") as game:
+            game.truncate()
+            game.write(gameState["flag"]["gameStarted"])
+            game.close()
+
+#open_file("./game.txt") WORKS MAC
