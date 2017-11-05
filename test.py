5c61d184a81f0ad162c48468e412a905f5dcee7f
LAPTOP-93OB3UD4\samwi <samwiston8@gmail.com>
2017-11-04 21:10:13 -0400

    Folders work now?

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import subprocess
 
 def open_file(path):
     if platform.system() == "Windows":
-        subprocess.Popen(r'explorer,' + path)
+        subprocess.Popen(r'explorer' + path)
     elif platform.system() == "Darwin":
         os.system('open "%s"' % "actions")
     else:
         subprocess.Popen(["xdg-open", path])
 
-open_file(".\\actions\\attack")
+open_file("\\actions")
