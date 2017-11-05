4ca92fd966c92bea08a0d3d6aeeff94351b489bd
LAPTOP-93OB3UD4\samwi <samwiston8@gmail.com>
2017-11-05 02:07:47 -0500

    Nick wants to tear down all of sam's work

--- creation.py
+++ creation.py
@@ -1,4 +1,9 @@
 import json
+import sys
+sys.path.insert(0,"./game/resources/")
+import core
+import os
+import platform
 
 def run():
     with open("character.json", "r+") as data:
@@ -9,10 +14,35 @@ def run():
         data.truncate()
 
 def create():
-    with open("character.json", "r+") as data:
+    with open("./game/resources/data.json", "r+") as data:
         gameState = json.load(data)
 
-        gameState["appearence"]["name"] = raw_input('What is your name? ')
+        path = "./name.txt"
+        writeFile = open(path, "w")
+        writeFile.close()
+
+        readFile = open(path, "r")
+
+        core.open_file(path)
+
+        reading = True
+
+        while reading:
+            name = readFile.readline()
+            if name != "":
+                reading = False
+
+        readFile.close()
+
+        if platform.system() == "Windows":
+            os.system("taskkill /f /im notepad.exe")
+        else:
+            os.system("pkill -9 \"TextEdit\"")
+
+        os.remove(path)
+
+
+        gameState["character"]["name"] = name
 
 
         data.seek(0)        # <--- should reset file position to the beginning.
