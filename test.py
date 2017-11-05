25e1369c39edb5e41d6e930a4ee2a87c70b667dd
LAPTOP-93OB3UD4\samwi <samwiston8@gmail.com>
2017-11-04 20:22:35 -0400

    Yes^2

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-import subprocess
+"""import subprocess
 import sys
+import platform
 
 
-if sys.platform == "win32":
+if platform.system() == "Windows":
     subprocess.Popen(r'explorer /select,".\actions\attack"')
 
 elif sys.platform == "darwin"
     #do some shit
+"""
+import os
+import platform
+import subprocess
+def open_file(path):
+    if platform.system() == "Windows":
+        subprocess.Popen(r'explorer /select,' + path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])
+
+open_file(".\\actions\\attack")
