69c4790213c82d6937357581f4918dd7076b3193
LAPTOP-93OB3UD4\samwi <samwiston8@gmail.com>
2017-11-04 23:01:04 -0400

    Replace is qweird

--- game/resources/core.py
+++ game/resources/core.py
@@ -5,21 +5,17 @@ import subprocess
 
 def open_file(path):
     if platform.system() == "Windows":
+        path = path.replace("/", "\\")
         subprocess.Popen('explorer "{0}"'.format(path))
     elif platform.system() == "Darwin":
+        path = path.replace("\\", "/")
         os.system('open "%s"' % "actions")
     else:
         subprocess.Popen(["xdg-open", path])
 
-<<<<<<< HEAD
 def run():
     with open("data.json") as data:
         gameState = json.load(data)
         print gameState["name"]
-=======
-with open("data.json") as data:
-    gameState = json.load(data)
-    print gameState["name"]
 
-def run():
->>>>>>> b03707551a74d87210386bc27151016fcc012e5c
+open_file("./game.txt")
