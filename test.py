457602a6833c267760a67b3819ffddc2beb22bfc
LAPTOP-93OB3UD4\samwi <samwiston8@gmail.com>
2017-11-04 20:11:27 -0400

    Yes

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import subprocess
-subprocess.Popen(r'explorer /select,".\actions\attack"')
+import sys
+
+
+if sys.platform == "win32":
+    subprocess.Popen(r'explorer /select,".\actions\attack"')
+
+elif sys.platform == "darwin"
+    #do some shit
