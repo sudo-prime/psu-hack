28fa05cf53754177af6f809c718ec59604899611
Nick <nicholas.vandette@nuix.com>
2017-11-05 04:09:42 -0500

    added player name functionality

--- game/readme.txt
+++ game/readme.txt
@@ -1,2 +1,4 @@
-if youre reading this,
+if youre reading this, you weren’t supposed to see the game folder!
 please dont cheat!
+love you,
+nick
--- game/resources/core.py
+++ game/resources/core.py
@@ -1,7 +1,8 @@
 import json
-import os
+import os, sys
 import platform
 import subprocess
+from shutil import copyfile
 
 def open_file(path):
     if platform.system() == "Windows":
@@ -17,7 +18,22 @@ def run():
     with open("./game/resources/data.json") as data:
         gameState = json.load(data)
 
+        # Name entry
+
+        if gameState["flag"]["nameConfirm"] == True:
+            path = "./name.txt"
+            writeFile = open(path, "w")
+            writeFile.close()
+
+            nameFile = open(path, "r")
+
+            copyfile("./game/resources/done.pyw", "done.pyw")
+
+        # Write result to game.txt
         with open("./game.txt", "r+") as game:
             game.truncate()
-            game.write(str(gameState[""]["gameStarted"]))
+            game.write("")
             game.close()
+
+if __name__ == "__main__":
+    run()
