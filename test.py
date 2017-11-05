c4211b300d43c1e79a8ad44cc436db5327ab2042
Nick <nicholas.vandette@nuix.com>
2017-11-04 20:39:53 -0400

    added cross-platform reveal file functionality

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import os
 import platform
 import subprocess
+
 def open_file(path):
     if platform.system() == "Windows":
-        subprocess.Popen(r'explorer /select,' + path)
+        subprocess.Popen(r'explorer,' + path)
     elif platform.system() == "Darwin":
-        subprocess.Popen(["open", path])
+        os.system('open "%s"' % "actions")
     else:
         subprocess.Popen(["xdg-open", path])
 
